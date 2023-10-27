10d87c9e52bc8f9a98507df7c61397a529104805
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 <wngns0812@gmail.com>
2023-10-28 00:23:01 +0900

    feat: 프로젝트 생성 / CI-CD 파일 추가

---
+++ src/main/java/com/example/balanceGame/BalanceGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.balanceGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceGameApplication.class, args);
+	}
+
+}
