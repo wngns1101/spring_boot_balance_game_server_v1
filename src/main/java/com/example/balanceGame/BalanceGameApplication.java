9b51ce7bf28ad933ca86f06e99b0a4675dc3dd60
wngns1101 <wngns0812@gmail.com>
2023-12-28 22:48:59 +0900

    chore: 서버 시간 설정 추가

--- src/main/java/com/example/balanceGame/BalanceGameApplication.java
+++ src/main/java/com/example/balanceGame/BalanceGameApplication.java
@@ -1,15 +1,21 @@
 package com.example.balanceGame;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BalanceGameApplication {
-
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BalanceGameApplication.class, args);
 	}
